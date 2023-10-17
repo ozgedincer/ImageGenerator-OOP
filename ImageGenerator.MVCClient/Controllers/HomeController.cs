f65e479185649e462d3625f43ce7b8f0cfd0f5b7
Özge Dinçer <dncerozge@gmail.com>
2023-10-17 13:54:18 +0300

    Proje dosyası ekle.

---
+++ ImageGenerator.MVCClient/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.Interfaces;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using System.Diagnostics;
+using ImageGenerator.MVCClient.Models;
+
+namespace ImageGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOpenAIService _openAiService;
+
+        public HomeController(ILogger<HomeController> logger, IOpenAIService openAiService)
+        {
+            _logger = logger;
+            _openAiService = openAiService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(HomeIndexViewModel viewModel)
+        {
+
+            var imageResult = await _openAiService.Image.CreateImage(new ImageCreateRequest
+            {
+                Prompt = viewModel.Prompt,
+                N = viewModel.ImageCount,
+                Size = StaticValues.ImageStatics.Size.Size512,
+                ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
+                User = "Cengizhan"
+            });
+
+            List<string> urls;
+
+            if (imageResult.Successful)
+            {
+                urls = imageResult.Results.Select(r => r.Url).ToList();
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SeriTarikGetir()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ImageGenerator.MVCClient/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddOpenAIService(settings => settings.ApiKey = "");
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
